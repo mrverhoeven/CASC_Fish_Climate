ad7eeed7f470a9a91574d43bba4706b0ea6c7bac
Michael Verhoeven <michael.verhoeven.mrv@gmail.com>
2023-06-12 13:40:44 -0500

    new fish file added, adds check of unique surveys between effort and catcth data (checks zeros)

--- Data_and_Scripts/Scripts/DNR data export review.R
+++ Data_and_Scripts/Scripts/DNR data export review.R
@@ -3,8 +3,11 @@
 
 library(data.table)
 library(dplyr)
+library(stringr)
+library(tidyr)
 
-wae_fish <- fread("C:\\Users\\verh0064\\Desktop\\wae_raw_allregions_2019-2022.txt", na.strings = "null")
+
+wae_fish <- fread("C:\\Users\\verh0064\\Desktop\\wae_raw_allregions_2019-2022_v02.txt", na.strings = "null")
 
 wae_catch <- fread("C:\\Users\\verh0064\\Desktop\\wae_cpe_allregions_2019-2022.txt", na.strings = "null")
 
@@ -26,7 +29,7 @@ wae_fish[ , .N , FISH_COUNT][order(FISH_COUNT)]
 wae_fish <- uncount(wae_fish, FISH_COUNT, .remove = T , .id = "uncount_id")
 
 #summarize by data avail:
-wae_fish[ , .N , .(year = year(date_clean) , catch = !is.na(CPUE), length = !is.na(LEN_MM), age = !is.na(OFF_AGE))][order(year)]
+wae_fish[ GEARTP %in% c("GN", "GSH", "GDE") , .N , .(year = year(date_clean) , catch = !is.na(CPUE), length = !is.na(LEN_MM), age = !is.na(OFF_AGE))][order(year)]
 
 wae_fish[ , .N , year(date_clean)]
 
@@ -36,9 +39,16 @@ wae_catch[ , date_clean := as.IDate(SURVEYDATE, format = "%m/%d/%Y") , ]
 
 wae_catch[ GEAR %in% c("GN", "GSH","GDE"), sum(TOTAL_CATCH) , .(year(date_clean), GEAR) ][order(year)]
 
+wae_catch[ , .N ,TOTAL_CATCH][ order(TOTAL_CATCH) , ]
+
 
 #effort
 colnames(wae_effort)
+
+wae_effort[, SURVEY_ID, ] %in% wae_catch[  ,SURVEY_ID , ] 
+
+wae_fish[ , .N , .(SRVY_DT, )]
+
 
 
 
